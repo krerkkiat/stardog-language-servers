b0e4802411a9733e0950d26b6fbd31c4591c389a
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2023-12-22 19:03:55 -0500

    feat: return the human name of BFO in hover and autocomplete

--- packages/sparql-language-server/src/SparqlLanguageServer.ts
+++ packages/sparql-language-server/src/SparqlLanguageServer.ts
@@ -5,6 +5,8 @@ import {
   StardogSparqlParser,
   W3SpecSparqlParser,
   sparqlKeywords,
+  isCstNode,
+  traverse,
 } from 'millan';
 import {
   ARBITRARILY_LARGE_NUMBER,
@@ -28,6 +30,7 @@ import {
   CompletionItem,
   CompletionItemKind,
   FoldingRangeRequestParam,
+  Hover,
   IConnection,
   InitializeParams,
   InitializeResult,
@@ -48,6 +51,108 @@ export class SparqlLanguageServer extends AbstractLanguageServer<
   private relationshipCompletionItems = [];
   private typeBindings = [];
   private typeCompletionItems = [];
+  private bfoMappings = {
+    'bfo:BFO_0000054': 'has realization',
+    'bfo:BFO_0000055': 'realizes',
+    'bfo:BFO_0000056': 'participates in at some time',
+    'bfo:BFO_0000057': 'has participant at some time',
+    'bfo:BFO_0000058': 'is concretized by at some time',
+    'bfo:BFO_0000059': 'concretizes at some time',
+    'bfo:BFO_0000062': 'preceded by',
+    'bfo:BFO_0000063': 'precedes',
+    'bfo:BFO_0000066': 'occurs in',
+    'bfo:BFO_0000082': 'located in at all times',
+    'bfo:BFO_0000084': 'generically depends on at some time',
+    'bfo:BFO_0000101': 'is carrier of at some time',
+    'bfo:BFO_0000108': 'exists at',
+    'bfo:BFO_0000110': 'has continuant part at all times',
+    'bfo:BFO_0000111': 'has proper continuant part at all times',
+    'bfo:BFO_0000113': 'has material basis at all times',
+    'bfo:BFO_0000115': 'has member part at some time',
+    'bfo:BFO_0000117': 'has occurrent part',
+    'bfo:BFO_0000118': 'has proper occurrent part',
+    'bfo:BFO_0000121': 'has temporal part',
+    'bfo:BFO_0000124': 'location of at some time',
+    'bfo:BFO_0000127': 'material basis of at some time',
+    'bfo:BFO_0000129': 'member part of at some time',
+    'bfo:BFO_0000132': 'occurrent part of',
+    'bfo:BFO_0000136': 'proper temporal part of',
+    'bfo:BFO_0000137': 'proper continuant part of at all times',
+    'bfo:BFO_0000138': 'proper occurrent part of',
+    'bfo:BFO_0000139': 'temporal part of',
+    'bfo:BFO_0000153': 'temporally projects onto',
+    'bfo:BFO_0000163': 'material basis of at all times',
+    'bfo:BFO_0000164': 'concretizes at all times',
+    'bfo:BFO_0000165': 'is concretized by at all times',
+    'bfo:BFO_0000166': 'participates in at all times',
+    'bfo:BFO_0000167': 'has participant at all times',
+    'bfo:BFO_0000170': 'location of at all times',
+    'bfo:BFO_0000171': 'located in at some time',
+    'bfo:BFO_0000172': 'has member part at all times',
+    'bfo:BFO_0000173': 'member part of at all times',
+    'bfo:BFO_0000174': 'has proper continuant part at some time',
+    'bfo:BFO_0000175': 'proper continuant part of at some time',
+    'bfo:BFO_0000176': 'continuant part of at some time',
+    'bfo:BFO_0000177': 'continuant part of at all times',
+    'bfo:BFO_0000178': 'has continuant part at some time',
+    'bfo:BFO_0000181': 'has proper temporal part',
+    'bfo:BFO_0000183': 'environs',
+    'bfo:BFO_0000184': 'history of',
+    'bfo:BFO_0000185': 'has history',
+    'bfo:BFO_0000194': 'specifically depended on by',
+    'bfo:BFO_0000195': 'specifically depends on',
+    'bfo:BFO_0000196': 'bearer of',
+    'bfo:BFO_0000197': 'inheres in',
+    'bfo:BFO_0000199': 'occupies temporal region',
+    'bfo:BFO_0000200': 'occupies spatiotemporal region',
+    'bfo:BFO_0000210': 'occupies spatial region at some time',
+    'bfo:BFO_0000211': 'occupies spatial region at all times',
+    'bfo:BFO_0000216': 'spatially projects onto at some time',
+    'bfo:BFO_0000217': 'spatially projects onto at all times',
+    'bfo:BFO_0000218': 'has material basis at some time',
+    'bfo:BFO_0000219': 'generically depends on at all times',
+    'bfo:BFO_0000220': 'is carrier of at all times',
+    'bfo:BFO_0000221': 'first instant of',
+    'bfo:BFO_0000222': 'has first instant',
+    'bfo:BFO_0000223': 'last instant of',
+    'bfo:BFO_0000224': 'has last instant',
+    'bfo:BFO_0000001': 'entity',
+    'bfo:BFO_0000002': 'continuant',
+    'bfo:BFO_0000003': 'occurrent',
+    'bfo:BFO_0000004': 'independent continuant',
+    'bfo:BFO_0000006': 'spatial region',
+    'bfo:BFO_0000008': 'temporal region',
+    'bfo:BFO_0000009': 'two-dimensional spatial region',
+    'bfo:BFO_0000011': 'spatiotemporal region',
+    'bfo:BFO_0000015': 'process',
+    'bfo:BFO_0000016': 'disposition',
+    'bfo:BFO_0000017': 'realizable entity',
+    'bfo:BFO_0000018': 'zero-dimensional spatial region',
+    'bfo:BFO_0000019': 'quality',
+    'bfo:BFO_0000020': 'specifically dependent continuant',
+    'bfo:BFO_0000023': 'role',
+    'bfo:BFO_0000024': 'fiat object part',
+    'bfo:BFO_0000026': 'one-dimensional spatial region',
+    'bfo:BFO_0000027': 'object aggregate',
+    'bfo:BFO_0000028': 'three-dimensional spatial region',
+    'bfo:BFO_0000029': 'site',
+    'bfo:BFO_0000030': 'object',
+    'bfo:BFO_0000031': 'generically dependent continuant',
+    'bfo:BFO_0000034': 'function',
+    'bfo:BFO_0000035': 'process boundary',
+    'bfo:BFO_0000038': 'one-dimensional temporal region',
+    'bfo:BFO_0000040': 'material entity',
+    'bfo:BFO_0000140': 'continuant fiat boundary',
+    'bfo:BFO_0000141': 'immaterial entity',
+    'bfo:BFO_0000142': 'fiat line',
+    'bfo:BFO_0000145': 'relational quality',
+    'bfo:BFO_0000146': 'fiat surface',
+    'bfo:BFO_0000147': 'fiat point',
+    'bfo:BFO_0000148': 'zero-dimensional temporal region',
+    'bfo:BFO_0000182': 'history',
+    'bfo:BFO_0000202': 'temporal interval',
+    'bfo:BFO_0000203': 'temporal instant',
+  };
 
   constructor(connection: IConnection) {
     // Unlike other servers, the Sparql server instantiates a different parser
@@ -56,6 +161,7 @@ export class SparqlLanguageServer extends AbstractLanguageServer<
   }
 
   onInitialization(params: InitializeParams): InitializeResult {
+    this.connection.onHover(this.handleHover.bind(this));
     this.connection.onCompletion(this.handleCompletion);
     this.connection.onFoldingRanges((params: FoldingRangeRequestParam) =>
       this.handleFoldingRanges(params, true, false)
@@ -322,20 +428,30 @@ export class SparqlLanguageServer extends AbstractLanguageServer<
       };
     });
 
-    const localCompletions: CompletionItem[] = localNames.map((local) => ({
-      label: local,
-      kind: CompletionItemKind.EnumMember,
-      sortText: candidates.some((tkn) =>
-        isLocalName(tkn.nextTokenType.tokenName)
-      )
-        ? `2${local}` // number prefix used to force ordering of suggestions to user
-        : null,
-      textEdit: this.replaceTokenAtCursor({
-        document,
-        tokenAtCursor,
-        replacement: local,
-      }),
-    }));
+    const localCompletions: CompletionItem[] = localNames.map((local) => {
+      const humanName = this.bfoMappings[local] || '';
+      let newLabel = '';
+      if (humanName !== '') {
+        newLabel = `${local} (${humanName})`;
+      } else {
+        newLabel = local;
+      }
+
+      return {
+        label: newLabel,
+        kind: CompletionItemKind.EnumMember,
+        sortText: candidates.some((tkn) =>
+          isLocalName(tkn.nextTokenType.tokenName)
+        )
+          ? `2${local}` // number prefix used to force ordering of suggestions to user
+          : null,
+        textEdit: this.replaceTokenAtCursor({
+          document,
+          tokenAtCursor,
+          replacement: local,
+        }),
+      };
+    });
 
     const iriCompletions: CompletionItem[] = iris.map((iri) => ({
       label: iri,
@@ -414,6 +530,100 @@ export class SparqlLanguageServer extends AbstractLanguageServer<
     }
 
     return finalCompletions;
+  }
+
+  handleHover(params: TextDocumentPositionParams): Hover {
+    const { uri } = params.textDocument;
+    const document = this.documents.get(uri);
+    const content = document.getText();
+    let { cst } = this.parseStateManager.getParseStateForUri(uri);
+
+    if (!cst) {
+      const { cst: newCst } = this.parseDocument(document);
+      cst = newCst;
+      this.parseStateManager.saveParseStateForUri(uri, { cst });
+    }
+
+    const offsetAtPosition = document.offsetAt(params.position);
+    const currentRuleTokens: IToken[] = [];
+    let cursorTkn: IToken;
+    let currentRule: string;
+
+    const tokenCollector = (ctx, next) => {
+      if (isCstNode(ctx.node)) {
+        return next();
+      }
+      currentRuleTokens.push(ctx.node);
+    };
+
+    const findCurrentRule = (ctx, next) => {
+      const { node, parentCtx } = ctx;
+      if (isCstNode(node)) {
+        return next();
+      }
+      // must be a token
+      if (
+        parentCtx.node &&
+        offsetAtPosition >= node.startOffset &&
+        offsetAtPosition <= node.endOffset
+      ) {
+        // found token that user's cursor is hovering over
+        cursorTkn = node;
+        currentRule = parentCtx.node.name;
+
+        traverse(parentCtx.node, tokenCollector);
+      }
+    };
+
+    traverse(cst, findCurrentRule);
+
+    // get first and last tokens' positions
+    const currentRuleRange = currentRuleTokens.reduce(
+      (memo, token) => {
+        if (token.endOffset > memo.endOffset) {
+          memo.endOffset = token.endOffset;
+        }
+        if (token.startOffset < memo.startOffset) {
+          memo.startOffset = token.startOffset;
+        }
+        return memo;
+      },
+      {
+        startOffset: content.length,
+        endOffset: 0,
+      }
+    );
+
+    if (!cursorTkn) {
+      return {
+        contents: [],
+      };
+    }
+
+    // Look up the BFO_ mapping for the current token.
+    const humanName = this.bfoMappings[cursorTkn.image] || '';
+
+    if (humanName !== '') {
+      return {
+        contents: `\`\`\`
+${currentRule} : '${humanName}'
+  \`\`\``,
+        range: {
+          start: document.positionAt(currentRuleRange.startOffset),
+          end: document.positionAt(currentRuleRange.endOffset + 1),
+        },
+      };
+    } else {
+      return {
+        contents: `\`\`\`
+${currentRule}
+  \`\`\``,
+        range: {
+          start: document.positionAt(currentRuleRange.startOffset),
+          end: document.positionAt(currentRuleRange.endOffset + 1),
+        },
+      };
+    }
   }
 
   onContentChange(
